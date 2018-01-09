7b0a5c338c79d597d35122c93b6f296f9dd50d4a
Tobias Fuhrimann <tobias.fuhrimann@gmail.com>
2018-01-09 09:34:03 +0100

    Update dependencies

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,14 +1,21 @@
 import fetch from "node-fetch";
 
-export function getResourceByPath(path: string, method = "GET", body?: any) {
-  return fetch(`https://jsonplaceholder.typicode.com${path}`, {
-    body: JSON.stringify(body),
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json",
-    },
-    method,
-  })
-    .then((res) => res.json())
-    .catch((err) => console.error(err));
+export async function getResourceByPath(
+  path: string,
+  method = "GET",
+  body?: any,
+) {
+  try {
+    const res = await fetch(`https://jsonplaceholder.typicode.com${path}`, {
+      body: JSON.stringify(body),
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      method,
+    });
+    return res.json();
+  } catch (err) {
+    console.error(err);
+  }
 }
