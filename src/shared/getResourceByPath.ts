2c8ba1923072af8ddda7321d121dc00ef0a1f184
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2019-03-10 11:29:36 +0100

    Replace tslint with eslint

--- src/shared/getResourceByPath.ts
+++ src/shared/getResourceByPath.ts
@@ -3,19 +3,15 @@ import fetch from "node-fetch";
 export const getResourceByPath = async (
   path: string,
   method = "GET",
-  body?: any,
-) => {
-  try {
-    const res = await fetch(`https://jsonplaceholder.typicode.com${path}`, {
-      body: JSON.stringify(body),
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-      },
-      method,
-    });
-    return res.json();
-  } catch (err) {
-    console.error(err);
-  }
+  body?: object,
+): Promise<object | object[]> => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com${path}`, {
+    method,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  return res.json();
 };
--- src/users/service.ts
+++ src/users/service.ts
@@ -1,14 +1,41 @@
 import { getResourceByPath } from "../shared/getResourceByPath";
 
-export const getUsers = () => getResourceByPath("/users");
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
 
-export const getUser = (id: number) => getResourceByPath(`/users/${id}`);
+export const getUsers = (): Promise<User[]> => getResourceByPath("/users");
 
-export const createUser = (newUser: any) =>
+export const getUser = (id: number): Promise<User> =>
+  getResourceByPath(`/users/${id}`);
+
+export const createUser = (newUser: User): Promise<void> =>
   getResourceByPath("/users", "POST", newUser);
 
-export const updateUser = (id: number, newFields: any) =>
-  getResourceByPath(`/users/${id}`, "PATCH", newFields);
+export const updateUser = (
+  id: number,
+  newFields: Partial<User>,
+): Promise<void> => getResourceByPath(`/users/${id}`, "PATCH", newFields);
 
-export const deleteUser = (id: number) =>
+export const deleteUser = (id: number): Promise<void> =>
   getResourceByPath(`/users/${id}`, "DELETE");
